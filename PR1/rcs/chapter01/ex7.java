ace75c3e2a9e3b53edaded5a2564b12e32c48a5f
quockhhanhchit <nguyenquockhanh9325@gmail.com>
2024-09-02 23:43:39 +0700

    Add files via upload

---
+++ PR1/rcs/chapter01/ex7.java
@@ -0,0 +1,14 @@
+public class ex7 {
+    public static void main(String[] args) {
+        System.out.println(" dan so nam thu nhat la :");
+        System.out.println(312032486 + ((365*60*60*24)/7) - ((365*60*60*24)/13) + ((365*60*60*24)/45));
+        System.out.println(" dan so nam thu hai la :");
+        System.out.println(((312032486 + ((365*60*60*24)/7) - ((365*60*60*24)/13) + ((365*60*60*24)/45)))*2);
+        System.out.println(" dan so nam thu ba la :");
+        System.out.println(((312032486 + ((365*60*60*24)/7) - ((365*60*60*24)/13) + ((365*60*60*24)/45)))*3);
+        System.out.println(" dan so nam thu bon la :");
+        System.out.println(((312032486 + ((365*60*60*24)/7) - ((365*60*60*24)/13) + ((365*60*60*24)/45)))*4);
+        System.out.println(" dan so nam thu nam la :");
+        System.out.println(((312032486 + ((365*60*60*24)/7) - ((365*60*60*24)/13) + ((365*60*60*24)/45)))*5);
+    }
+}
---
+++ PR1/rcs/chapter01/ex8.java
@@ -0,0 +1,8 @@
+public class ex8 {
+    public static void main(String[] args) {
+       System.out.println(" gia tri cua x la:");
+        System.out.println((44.5*5.5 - 50.2*5.9)/(3.4*5.5-50.2*2.1));
+        System.out.println(" gia tri cua y la:");
+        System.out.println((34*5.9-44.5*2.1)/(3.4*5.5-50.2*2.1));
+    }
+}
